7522b459cde944ff63e3529cc6819ad954e9d785
johnliu <johnliutw@hotmail.com>
2023-05-28 16:03:07 +0800

    Part2: Fetch data

--- Ptt_crawler.py
+++ Ptt_crawler.py
@@ -14,6 +14,23 @@ try:
     # 開啟 ptt 網頁
     driver.get('https://www.ptt.cc/bbs/Stock/index.html')
 
+    # 解析畫面
+    sourceCode = BeautifulSoup(driver.page_source, 'html.parser')
+    metaSection = sourceCode.select('div.r-list-container')[0]
+    sections = metaSection.select('div.r-ent')
+    for section in sections:
+        # 抓取資料
+        title = section.select('div.title')[0].text
+        num = section.select('div.nrec')[0].text
+        author = section.select('div.author')[0].text
+        date = section.select('div.date')[0].text
+
+        # 顯示資料
+        title = title.strip()
+        print(title)
+        print(num)
+        print(author)
+        print(date)
 except Exception as e:
     traceback.print_exc()
     driver.close()
