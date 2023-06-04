c925d40ca799ea9cf1df1027b791c135eb51f36a
johnliu <johnliutw@hotmail.com>
2023-06-04 12:07:45 +0800

    Part2: Fetch data

--- Ebc_crawler.py
+++ Ebc_crawler.py
@@ -8,7 +8,7 @@ import traceback
 from datetime import date
 today = date.today()
 # 修改日期格式
-search_date = today.strftime('%Y-%m-%d')
+search_date = today.strftime('%m/%d')
 
 # 開啟瀏覽器
 options = Options()
@@ -19,6 +19,20 @@ try:
   # 開啟東森網站
   driver.get('https://news.ebc.net.tw/news/living')
   sourceCode = BeautifulSoup(driver.page_source, "html.parser")
+
+  # 爬取資料
+  article_box = sourceCode.select('div.news-list-box')[0]
+  articles = article_box.select('div.style1.white-box')
+  for article in articles:
+      date_string = article.select('span.small-gray-text')[0].text
+      if search_date not in date_string:
+        continue
+
+      title = article.select('span.title')[0].text
+      summary = article.select('span.summary')[0].text
+      print(title)
+      print(date_string)
+      print(summary)
 
 except Exception as e:
   traceback.print_exc()
