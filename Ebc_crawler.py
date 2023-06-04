3e7b92560f1e8c32ed007789b599c92bfe63be9c
johnliu <johnliutw@hotmail.com>
2023-06-04 23:55:42 +0800

    Part4: Write data to file

--- Ebc_crawler.py
+++ Ebc_crawler.py
@@ -6,10 +6,14 @@ from bs4 import BeautifulSoup
 import os
 import traceback
 
+import csv
+from docx import Document
+
 from datetime import date
 today = date.today()
 # 修改日期格式
 search_date = today.strftime('%m/%d')
+file_date = today.strftime('%Y-%m-%d')
 
 # 設定無圖片與影片讀取模式
 options = Options()
@@ -22,6 +26,12 @@ driver = webdriver.Chrome(service=service, options=options)
 try:
   # 開啟東森網站
   driver.get('https://news.ebc.net.tw/news/living')
+  # 建立 Word 檔案
+  document = Document()
+  document.add_heading(file_date+' 東森資料', 0)
+  # 建立 Csv 檔案
+  csv_file = open(file_date+'_data.csv', 'a', newline='', encoding='utf_8_sig')
+  writer = csv.writer(csv_file)
   for index in range(1, 4):
     sourceCode = BeautifulSoup(driver.page_source, "html.parser")
 
@@ -39,11 +49,25 @@ try:
         print(date_string)
         print(summary)
 
+        # 寫入 Csv
+        writer.writerow([title, date_string, summary])
+
+        # 寫入 Word
+        document.add_heading(title, 1)
+        paragraph = document.add_paragraph('')
+        paragraph.add_run(date_string).bold = True
+        document.add_paragraph(summary)
+
     # 模擬動態換頁按鈕點擊
     element = driver.find_elements(By.CSS_SELECTOR, 'div.page-area a.white-btn')[-1]
     driver.execute_script("arguments[0].scrollIntoView();", element)
     element.click()
 
+  # 關閉與儲存 csv + word 檔案
+  csv_file.close()
+  document.save(file_date+' 東森資料.docx')
+
 except Exception as e:
+  csv_file.close()
   traceback.print_exc()
   driver.close()
