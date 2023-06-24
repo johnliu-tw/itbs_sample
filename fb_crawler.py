ca5e3a84e1954e599a5f61697e519873245f1262
johnliu <johnliutw@hotmail.com>
2023-06-24 16:20:47 +0800

    Part2: result file init

--- fb_crawler.py
+++ fb_crawler.py
@@ -1,11 +1,37 @@
 from facebook_scraper import get_posts
+import csv
+from datetime import date
 import traceback
+today = date.today()
+file_date = today.strftime('%Y-%m-%d')
+
+reaction_map = {
+    'like': '讚',
+    'love': '大心',
+    'haha': '哈',
+    'sorry': '嗚',
+    'wow': '哇',
+    'angry': '怒',
+    'care': '加油'
+}
+
+
+csv_file = open(file_date+'_data.csv', 'a', newline='', encoding='utf_8_sig')
+writer = csv.writer(csv_file)
+writer.writerow(['日期', '內容', '分享數', '反應:讚', '反應:大心', '反應:哈', '反應:嗚', '反應:哇', '反應:怒', '反應:加油', 
+                 '留言', '留言反應:讚', '留言反應:大心', '留言反應:哈', '留言反應:嗚', '留言反應:哇', '留言反應:怒', '留言反應:加油',
+                 '留言回覆', '回覆反應:讚', '回覆反應:大心', '回覆反應:哈', '回覆反應:嗚', '回覆反應:哇', '回覆反應:怒', '回覆反應:加油'])
 
 try:
-  for post in get_posts('Gooaye', pages=1, cookies='www.facebook.com_cookies.txt', extra_info=True, options={'comments': True}):
+  for post in get_posts('Gooaye', pages=1, cookies='www.facebook.com_cookies.txt', extra_info=True):
     date_string = post['time'].strftime('%Y-%m-%d %H:%M:%S')
 
     print(date_string)
     print(post['text'])
+    post_data = [date_string, post['text'], post['shares']]
+    writer.writerow(post_data)
+  
+  csv_file.close()
 except Exception as e:
+  csv_file.close()
   traceback.print_exc()
